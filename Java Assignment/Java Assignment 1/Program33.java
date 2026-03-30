b3098070b88232f485c275930983aa481e46b1ca
parthdarode <parth.darode.cmfeb26@gmail.com>
2026-03-30 13:29:43 +0530

    Delete Java Assignment/Java Assignment 1 directory

--- Java Assignment/Java Assignment 1/Program33.java
+++
@@ -1,9 +0,0 @@
-class Program33 {
-    public static void main(String[] args) {
-        int x = 10;
-
-        if(x == 10) {   // use == for comparison
-            System.out.println("Hello");
-        }
-    }
-}
